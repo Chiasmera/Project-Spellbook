1cb739fabad13116ec2f7d02e1a32b389bfd184d
Chiasmera <juliusseerup@gmail.com>
2022-02-14 21:34:59 +0100

    Spellgem Creation
    created basic spellgem class functionality

---
+++ Assets/Scripts/Spellgem.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spellgem : MonoBehaviour
+{
+    //Fields
+    private int id;
+    private int level;
+    private string name;
+    private string school;
+    private string god;
+    private bool isLearned;
+
+    //Constructor
+    public Spellgem (int id, int level, string name, string school, string god)
+    {
+        this.id = id;
+        this.level = level;
+        this.name = name;
+        this.school = school;
+        this.god = god;
+
+        SetSchoolColor();
+    }
+
+    //Methods
+    public void SetSchoolColor ()
+    {
+        switch (school)
+        {
+            case "Abjuration":
+                this.GetComponent<SpriteRenderer>().color = Color.blue;
+                break;
+            case "Illusion":
+                this.GetComponent<SpriteRenderer>().color = new Color(0.5f,0,1,1);
+                break;
+            case "Conjuration":
+                this.GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.64f, 0.0f, 1.0f);
+                break;
+            case "Enchantment":
+                this.GetComponent<SpriteRenderer>().color = Color.yellow;
+                break;
+            case "Evocation":
+                this.GetComponent<SpriteRenderer>().color = Color.red;
+                break;
+            case "Divination":
+                this.GetComponent<SpriteRenderer>().color = Color.white;
+                break;
+            case "Necromancy":
+                this.GetComponent<SpriteRenderer>().color = Color.black;
+                break;
+            case "Transmutation":
+                this.GetComponent<SpriteRenderer>().color = Color.green;
+                break;
+            default:
+                print("No school");
+                break;
+        }
+    }
+
+    public void SetLearned  (bool state)
+    {
+        isLearned = state;
+    }
+
+    public void ToggleLearned ()
+    {
+        if (isLearned == false)
+        {
+            SetLearned(true);
+        } else
+        {
+            SetLearned(false);
+        }
+    }
+
+    public bool GetLearned ()
+    {
+        return isLearned;
+    }
+}
